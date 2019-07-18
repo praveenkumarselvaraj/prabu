c934fa53696514a22940f2ced24d2604ae3684f9
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-18 17:29:14 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class num
+{
+    public static void main(String arv[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int sum=0;
+       for(int i=1;i<=n;i++)
+       {
+           sum=sum+i;
+       }
+       System.out.print(sum);
+    }
+}
+
