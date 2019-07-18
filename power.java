720e297667c914cc6dbab851ec231db39a231696
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-18 17:53:26 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+class power
+{
+    public static void main(String arv[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int n1=sc.nextInt();
+       System.out.print((int)Math.pow(n,n1));
+    }
+}
+
