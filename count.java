a685aaeb591032d4aded7e6d8a1e7ec74df3ddb6
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-18 17:46:53 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class count
+{
+    public static void main(String arv[])
+    {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       int count=0,rem;
+        while(n>0)
+        {
+            rem=n%10;
+            n/=10;
+            count++;
+        }
+       System.out.print(count);
+    }
+}
+
