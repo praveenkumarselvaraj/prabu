fe936ba86b5a57f13aa1db69df8a4fb48262d64f
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-18 18:13:58 +0530

    Add files via upload

---
+++ bodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class bodd
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        for(int i=n+1;i<n1;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
+
