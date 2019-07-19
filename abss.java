9c89b3da4bb33cb44a2276a2f7f52d34cbce52bf
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-19 17:40:30 +0530

    Add files via upload

---
+++ abss.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class abss {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int n3=sc.nextInt();
+        int nn=n-n2;
+        int nnn=n1-n3;
+         System.out.print(Math.abs(nn)+" "+Math.abs(nnn));
+    }
+}
