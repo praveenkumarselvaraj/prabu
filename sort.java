f8e06d3d9b40c499f67b6d27111c9bfbcf5a88ff
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-19 15:47:53 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Aray{ 
+    
+    public static void main(String args[]) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt(); 
+        int ar[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            ar[i]=sc.nextInt();
+        }
+        Arrays.sort(ar);
+        for(int i=0;i<n;i++)
+        {
+        System.out.print(ar[i]+" ");
+        }
+    } 
+} 
