83c2e180ee308e0b48a0c9c9dee0e9c2a8dd0e6b
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-19 11:09:31 +0530

    Add files via upload

---
+++ nmul.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class nmul
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        //int fact=1;
+        int n;
+        for(int i=1;i<=n1;i++)
+        { 
+            n=i;
+            n=n*n1;
+        System.out.print(n+" ");
+        }
+    }
+    
+}
+
