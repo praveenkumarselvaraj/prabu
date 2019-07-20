a0ec66b4598a960687adbda212be29d9b1e94656
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-20 09:51:26 +0530

    Add files via upload

---
+++ space (1).java
@@ -0,0 +1,12 @@
+import java.util.*;
+class space
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+       String ss[]=s.split(" ");
+        int l=ss.length;
+        System.out.print(l);
+    }
+}
