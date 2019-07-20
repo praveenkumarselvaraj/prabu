646a48911bb2be7017cef3bcbe1e2ef44004b84d
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-20 11:07:31 +0530

    Add files via upload

---
+++ cmd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class cmd{ 
+    
+    public static void main(String args[]) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        //int a = sc.nextInt(); 
+        int a[]=new int[args.length];
+        for(int i=0;i<args.length;i++)
+        {
+            a[i]= Integer.valueOf(args[i]);
+        }
+        Arrays.sort(a);
+        int l=args.length;
+        
+        System.out.print(a[l-1]);
+    } 
+} 
+
