d186389609fe6414c8f033671d1a346a2f1606e6
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-20 11:27:33 +0530

    Add files via upload

---
+++ concatt.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class concatt{ 
+    
+    public static void main(String args[]) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        String a = sc.next(); 
+        String b = sc.next(); 
+        String c=a+b;
+        System.out.print(c);
+    } 
+} 
+
+
