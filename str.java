afcc976e287a211ccee9defb6038c4b088019a0b
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-20 11:15:48 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class str{ 
+    
+    public static void main(String args[]) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        String a = sc.next(); 
+        int a1=sc.nextInt();
+        for(int i=1;i<=a1;i++)
+        {
+           System.out.println(a);
+        }
+    } 
+} 
+
