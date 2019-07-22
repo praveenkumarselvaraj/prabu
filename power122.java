a5f17f0c295eb3fc03147a0d38d6614afd40d9ac
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-22 12:30:14 +0530

    Add files via upload

---
+++ power122.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class power
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       if(n%2==0)
+        System.out.print("yes");
+        else
+        System.out.print("no");
+    }
+}
