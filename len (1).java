6fa41394ee4d33548db1fa518a59e8139a14f6f2
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-22 10:50:06 +0530

    Add files via upload

---
+++ len (1).java
@@ -0,0 +1,11 @@
+import java.util.*;
+class len
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+       // String d=String.valueOf(n);
+        System.out.print(n+1);
+    }
+}
