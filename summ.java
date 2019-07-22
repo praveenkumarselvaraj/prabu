2d9bb02c225a8cd26fbcfa6f64e7ae1e5a2eaa0e
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-22 12:23:15 +0530

    Add files via upload

---
+++ summ.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class summ
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        System.out.print(n+n1);
+    }
+}
