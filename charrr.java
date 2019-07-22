7b7d08c6da8cdc29bf99940e23796003352d0d1d
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-22 15:40:22 +0530

    Add files via upload

---
+++ charrr.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class charrr
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+        System.out.print(a.charAt(i));
+        }
+    }
+}
+
+
