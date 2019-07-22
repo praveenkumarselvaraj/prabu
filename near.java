b59a5e7a76aa5f8c6a9fd783558b9e688de4b415
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-22 14:24:49 +0530

    Add files via upload

---
+++ near.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class near
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //int n1=sc.nextInt();
+        if(n%2==0)
+        {
+        System.out.print(n);
+        }
+        else
+         System.out.print(n-1);
+    }
+}
+
