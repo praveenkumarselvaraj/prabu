d2b0a8081796e7260b62c304f5710b5d1ef93402
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-22 18:23:23 +0530

    Add files via upload

---
+++ seve.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class seve{ 
+    
+    public static void main(String args[]) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt(); 
+        if(a%7==0)
+        {
+        System.out.print("yes");
+        }
+        else
+        System.out.print("no");
+    } 
+} 
+
+
