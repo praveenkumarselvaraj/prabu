3d061489bf89232dca3574b732c7cba97f438cae
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-24 09:45:51 +0530

    Add files via upload

---
+++ cee.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+class cee
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float s=sc.nextFloat();
+       
+        System.out.println((int)Math.ceil(s));
+    }  
+}
+
