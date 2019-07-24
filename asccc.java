aef0d9673979c44121dd015be341769e4289f273
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-24 12:18:30 +0530

    Add files via upload

---
+++ asccc.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class asccc {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int ascii = c;
+		System.out.println(ascii);
+}
+} 
