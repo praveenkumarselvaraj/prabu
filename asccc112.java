c24335c8a5e4043cd3b35985c13d0b44db995db6
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-24 12:30:57 +0530

    Add files via upload

---
+++ asccc112.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.math.*;
+public class asccc112 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		float a=sc.nextFloat();
+		float b=sc.nextFloat();
+		System.out.printf("%.5f",a*b);
+} 
+}
