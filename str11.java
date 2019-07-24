86b9aa119b190a094bef5dd1b5ed2956803ad305
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-24 12:16:08 +0530

    Add files via upload

---
+++ str11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class str11 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char [] c = s.toCharArray();
+		for(int i=0;i<c.length;i++){
+			if(Character.isDigit(c[i])){
+				System.out.print(c[i]);
+			}
+		}
+}
+}
