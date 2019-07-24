1f13b00b97a155bc87f5dc885e395890b8d06fae
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-24 10:22:36 +0530

    Add files via upload

---
+++ mul13.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class mul13
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //for(int i=1;i<=n;i++)
+        //{
+            if(n%13==0)
+            {
+                System.out.print("yes");
+            }
+            else
+            System.out.print("no");
+        //}
+    }
+}
+
+
