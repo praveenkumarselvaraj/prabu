5fa6fa400a99e4d4e5c5e154bf128f2c9684f943
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-24 09:35:29 +0530

    Add files via upload

---
+++ between.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class between
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+         int a=sc.nextInt();
+         int b=sc.nextInt();
+        if(a<s && s<b)
+        System.out.println("yes");
+        else
+        System.out.println("no");
+    }
+}
+
