f90518ec801e838abced5933d2b228784d69d5f7
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-25 12:23:15 +0530

    Add files via upload

---
+++ smal11.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class smal11 {
+	public static void main(String[] args) {
+		int n;
+		Scanner	sc = new Scanner(System.in);		
+	    n = sc.nextInt();
+	    int s = sc.nextInt();
+	    int a[] = new int[n];
+	    for(int i=0;i<n;i++){
+		    a[i] = sc.nextInt();
+	}
+	    
+	  System.out.println(a[s]);
+	
+	}}
