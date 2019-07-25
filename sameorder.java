76c12cc02a06ada92647679d0888ff1b4d5f6c0f
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-25 11:54:50 +0530

    Add files via upload

---
+++ sameorder.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sameorder {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		int n = sc.nextInt();
+		String p="";
+		char [] c = str1.toCharArray();
+		for(int i = c.length-1;i>=0;i--){
+			p = p + c[i];
+		}
+		System.out.println(p.substring(0,n));
+
+}
+}
