090574bff54e43dffad0c488acd7408fb13a2a23
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-25 10:34:00 +0530

    Add files via upload

---
+++ nth.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class nth{
+	public static void main(String[] args) {
+		int n;
+		Scanner	sc = new Scanner(System.in);		
+	    n = sc.nextInt();
+	    int s = sc.nextInt();
+	    int a[] = new int[n];
+	    for(int i=0;i<n;i++){
+		    a[i] = sc.nextInt();
+	}
+	  System.out.println(a[s-1]);
+
+		}
+}
