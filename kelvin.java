5fdfbc13ab4d43c8cf396486ca62c0338522ad7a
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-25 10:29:49 +0530

    Add files via upload

---
+++ kelvin.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class kelvin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    int c = sc.nextInt();
+	    int k =0;
+	  System.out.println(k = c +273);
+				}
+}
