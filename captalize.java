f5c2cd334f19ef4c81e77272254864a2def84653
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-25 12:02:43 +0530

    Add files via upload

---
+++ captalize.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class captalize {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		for(String i : s.split(" ")){
+		String r = i.substring(0, 1);
+		System.out.print(r.toUpperCase() + i.substring(1).toLowerCase()+" ");
+		}
+}
+}
