9e5e540b5f3a64e681b8aecb17358bedc1a98dc3
praveenkumarselvaraj <34643070+praveenkumarselvaraj@users.noreply.github.com>
2019-07-25 12:09:19 +0530

    Add files via upload

---
+++ concat12 (1).java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class concat12 {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		String k =sc.next();
+	System.out.println(n+k);
+}
+}
